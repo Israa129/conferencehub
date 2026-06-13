68ed2bc8469655c03dbfe68dd109f7c6269fa8ca
Israa129 <israakhatib12@gmail.com>
2026-06-13 16:12:43 +0200

    Connect dashboard to conferences API

--- frontend/src/app/features/participant/dashboard/dashboard.ts
+++ frontend/src/app/features/participant/dashboard/dashboard.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
+
+import { ConferenceService } from '../../../services/conference.service';
+import { Conference } from '../../../models/conference';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,9 +12,27 @@ import { CommonModule } from '@angular/common';
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
-export class Dashboard {
+export class Dashboard implements OnInit {
+
   user = {
     nom: 'Amara',
     date: new Date()
   };
+
+  conferences: Conference[] = [];
+
+  constructor(private conferenceService: ConferenceService) {}
+
+  ngOnInit(): void {
+
+    this.conferenceService.getConferences().subscribe({
+      next: (data) => {
+        this.conferences = data;
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+
+  }
 }
